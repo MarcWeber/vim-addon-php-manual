acd9f4653419838d3ee37b7598a60edacace6edd
Marc Weber <marco-oweber@gmx.de>
2011-07-16 15:45:06 +0200

    manual update

--- manual/makeTags.sh
+++ manual/makeTags.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-# vi: %s/^\([^ ]*\)\n\n\s*(PHP/\1 ((PHP/g
-exuberant-ctags --langdef=A  '--regex-A=/([a-zA-Z_]*).*\(\(PHP/\1/' --language-force=A allinonemod.txt
+# older manual:  vi: %s/^\([^ ]*\)\n\n\s*(PHP/\1 ((PHP/g
+# exuberant-ctags --langdef=A  '--regex-A=/([a-zA-Z_]*).*\(\(PHP/\1/' --language-force=A allinonemod.txt
+
+# current manual: vi: %s@^\s\+-\{10,}\n\n\s\+-\{10,}\n\n \{10,50}\(\S*\)@thing: \1@g
+ctags --langdef=A  '--regex-A=/^thing: (.*)/\1/t,thing/' --language-force=A allinonemod.txt
